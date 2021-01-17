6e733b10ab2b81c17199edbdd71797e89d2ee96f
Jiaan <jiehanandika@gmail.com>
2021-01-18 01:28:30 +0700

    Update connect.php

--- function/connect.php
+++ function/connect.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'ghinaj');
+$conn = mysqli_connect('hwr4wkxs079mtb19.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	', 's5lr4zb4em84udc7', 'uy27lret1s9g774c', 'b67djmmzvukk0qot');
 ?>
